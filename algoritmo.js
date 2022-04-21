8e4c0e341ab0eac0bbb742203847e1184ea087cf
tobias gimenez~ <tobiasgimenezchirino@gmail.com~>
2022-04-21 11:33:22 -0300

    Termine la practica 1-7

---
+++ algoritmo.js
@@ -0,0 +1,21 @@
+var edadActual = 18; //Int
+var partidosInvictosRacing = 10;
+var nombreJugadorFavoritoCs = "Luken"; // String
+var comoEstas = "Bien, vos?";
+var tenesFiebre = false; //Bool
+var teGustaElCafe = true
+var cantKmRecorridosHoy = 2.3; //Float
+var distanciaMcaminadosHoy = 2300.10
+
+
+
+document.write(`
+    <h4> Edad actual: ${ edadActual } </h4>
+    <h4> Partidos invictos de Racing: ${ partidosInvictosRacing } </h4>
+    <h4> Nombre jugador favorito de CS: ${ nombreJugadorFavoritoCs } </h4>
+    <h4> ¿Como estas? ${ comoEstas } </h4>
+    <h4> Tenes fiebre: ${ tenesFiebre } </h4>
+    <h4> Te gusta el cafe: ${ teGustaElCafe } </h4>
+    <h4> Cuantos km recorriste hoy? ${ cantKmRecorridosHoy } </h4>
+    <h4> Distancia de M recorridos hoy? ${ distanciaMcaminadosHoy } </h4>
+`)
